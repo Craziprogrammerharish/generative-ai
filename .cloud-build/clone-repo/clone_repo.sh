687973dfaffa1eac6e01841bc7ad52474e2268a7
Holt Skinner <holtskinner@google.com>
2025-05-08 14:52:51 -0500

    ci: Update testing to only clone the most recent commit on main branch

--- .cloud-build/clone-repo/clone_repo.sh
+++ .cloud-build/clone-repo/clone_repo.sh
@@ -4,7 +4,7 @@
 OUTPUT_URI=$(cat OUTPUT_URI)
 
 # Clones the generative-ai repository from GitHub.
-git clone https://github.com/GoogleCloudPlatform/generative-ai.git
+git clone --depth 1 -b main https://github.com/GoogleCloudPlatform/generative-ai.git
 
 # Changes the current directory to the cloned repository.
 cd generative-ai || exit 1
